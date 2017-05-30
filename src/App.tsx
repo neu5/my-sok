98e26c39bcf92f88974c996dc13decec5ef3c4e8
Jakub Kasprzyk <neus90@gmail.com>
2017-05-30 17:53:15 +0200

    Add integration with contentful

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
 import * as React from 'react';
 import './App.css';
+import * as config from './config.env.local';
 
-const logo = require('./logo.svg');
+import { createClient } from 'contentful';
+
+const ITEM_NAME = 'Post';
 
 class App extends React.Component<{}, null> {
+  public client:any;
+  public state:any;
+
+  constructor() {
+    super();
+
+    this.client = this.getClient();
+    this.state = {
+      posts: this.getPosts()
+    }
+  }
+
+  getClient() {
+    return createClient(config);
+  }
+
+  getPosts() {
+    return this.client.getContentTypes()
+      .then(this.getPostsContentId)
+      .then(this.fetchPosts.call(this));
+  }
+
+  getPostsContentId(data:any) {
+    return data.items.find((item:any) => {
+      return item.name === ITEM_NAME;
+    }).sys.id;
+  }
+
+  fetchPosts(contentId:string) {
+    return this.client
+      .getEntries({'sys.id': contentId})
+      .then((resp:any) => console.log(resp));
+  }
+ 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          
         </p>
       </div>
     );
