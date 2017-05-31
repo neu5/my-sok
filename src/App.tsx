5d284e9c38c13fe31628e3223c8036ea3ed376fd
Jakub Kasprzyk <neus90@gmail.com>
2017-05-31 14:28:05 +0200

    Get test entries from contentful and print them on the page

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,69 @@ import { createClient } from 'contentful';
 const ITEM_NAME = 'Post';
 
 class App extends React.Component<{}, null> {
-  public client:any;
-  public state:any;
+  public client: any;
+  public state: any;
 
   constructor() {
     super();
 
     this.client = this.getClient();
     this.state = {
-      posts: this.getPosts()
-    }
+      posts: []
+    };
   }
 
   getClient() {
     return createClient(config);
   }
 
-  getPosts() {
-    return this.client.getContentTypes()
-      .then(this.getPostsContentId)
-      .then(this.fetchPosts.call(this));
+  componentDidMount() {
+    this.client.getContentTypes()
+      .then((contentTypes: any) => {
+        this.getPosts(this.getPostsContentTypeId(contentTypes));
+      });
   }
 
-  getPostsContentId(data:any) {
-    return data.items.find((item:any) => {
+  getPostsContentTypeId(data: any) {
+    return data.items.find((item: any) => {
       return item.name === ITEM_NAME;
     }).sys.id;
   }
 
-  fetchPosts(contentId:string) {
-    return this.client
-      .getEntries({'sys.id': contentId})
-      .then((resp:any) => console.log(resp));
+  getPosts(contentTypeId: string) {
+    this.client.getEntries({
+      content_type: contentTypeId
+    })
+      .then((entries: any) => {
+        this.setState({
+          posts: entries.items
+        });
+      });
+  }
+
+  renderPosts(posts: any) {
+    return posts.map((post: any, key: number) => {
+      return (
+      <div key={key}>
+        <h1>
+          {post.fields.title}
+        </h1>
+        <p>
+          {post.fields.body}
+        </p>
+      </div>
+      );
+    });
   }
  
   render() {
+    const posts = this.renderPosts(this.state.posts);
+
     return (
       <div className="App">
-        <div className="App-header">
-          
+        <div>
+          {posts}
         </div>
-        <p className="App-intro">
-          
-        </p>
       </div>
     );
   }
